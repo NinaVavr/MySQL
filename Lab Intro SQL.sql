cc3691d7e9ff896edcd0cb37e6484a63e8b7ed53
NinaVavr <ninavavr.kal@hotmail.com>
2023-05-16 17:51:49 +0200

    file added

---
+++ Lab Intro SQL.sql
@@ -0,0 +1,9 @@
+USE sakila;
+SELECT * FROM actor, film, customer;
+SELECT title FROM film;
+SELECT name FROM language;
+SELECT name AS language FROM language;
+SELECT COUNT(store_id) AS store_id FROM store;
+SELECT count(first_name) FROM staff;
+SELECT first_name FROM staff;SELECT count(*) from store;
+
